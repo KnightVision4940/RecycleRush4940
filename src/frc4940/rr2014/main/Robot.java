924dd5ec722d1fd2a7e191efcfd8f5e8c93abf05
StewEsho <StewardEsho@gmail.com>
2015-02-01 18:04:27 -0500

    Added improved Deadzone
    Improved the deadzone to fix juttering of motors (UNTESTED)
    Only enables robot rotation when trigger is held
    (UNTESTED)

--- src/frc4940/rr2014/main/Robot.java
+++ src/frc4940/rr2014/main/Robot.java
@@ -1,13 +1,15 @@
-
 package frc4940.rr2014.main;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
 
 public class Robot extends IterativeRobot {
+	//Declares the Subsystems and other global variables
 	XtremePro joystick = new XtremePro();
 	Talons motors = new Talons();
 	RobotDrive chassis = new RobotDrive(motors.Bae4, motors.Bae3, motors.Bae1, motors.Bae2);
-    /**
+	final int NULL = 0;
+    
+	/**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
@@ -26,7 +28,6 @@ public class Robot extends IterativeRobot {
      */
     public void teleopPeriodic() {
     	
-    	final int GyroAngle = 0;
     	//moves motor bases on twist
     	/*if(joystick.getTwist()>= 0.1 || joystick.getTwist()<= -0.1){
     		//motors.setBae1(joystick.getTwist());
@@ -42,7 +43,17 @@ public class Robot extends IterativeRobot {
     		motors.setBae1(0.5);
     	}*/
     	
-    	chassis.mecanumDrive_Cartesian(joystick.getXAxis() , joystick.getYAxis(), joystick.getTwist(), GyroAngle);
+    	//checks if the joystick is in the dead zone. If so, the motors are stopped. Else, the motors work bases on the input
+    	if(((-0.1 <= joystick.getXAxis()) && (joystick.getXAxis() <= 0.1)) || ((-0.1 <= joystick.getYAxis()) && (joystick.getYAxis() <= 0.1))){
+    		chassis.mecanumDrive_Cartesian(0, 0, 0, 0);
+    	} else {
+    		//checks if trigger is held, and uses twist rotation if true
+    		if(joystick.getTrigger()){
+    			chassis.mecanumDrive_Cartesian(joystick.getXAxis() , joystick.getYAxis(), joystick.getTwist(), NULL); //drives and rotates
+    		} else {
+    			chassis.mecanumDrive_Cartesian(joystick.getXAxis() , joystick.getYAxis(), NULL, NULL); //drives only, no rotation
+    		}
+    	}
     	
     }
     
